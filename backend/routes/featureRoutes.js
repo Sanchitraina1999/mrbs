c37542fee985b0df909ca30bbe8e5e38f0062e89
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 11:32:30 +0530

    update: master

--- backend/routes/featureRoutes.js
+++ backend/routes/featureRoutes.js
@@ -1,9 +1,7 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts, getProductById } from '../controllers/productController.js'
+import { getProducts } from '../controllers/featureController.js'
 
-router.route('/').get(getProducts)
-
-router.route('/:id').get(getProductById)
+router.route('/').get(getFeatures)
 
 export default router
