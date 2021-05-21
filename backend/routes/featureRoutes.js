389e09893d7bba200a629d26c8c4463eb659d7a3
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 12:09:25 +0530

    update: master

--- backend/routes/featureRoutes.js
+++ backend/routes/featureRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts } from '../controllers/featureController.js'
+import { getFeatures } from '../controllers/featureController.js'
 
 router.route('/').get(getFeatures)
 
