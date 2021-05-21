917533bc8b112a7f6c9542a027d5dbe5394e0cc5
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 15:37:37 +0530

    update: master

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -42,8 +42,8 @@ const LoginScreen = ({ location, history }) => {
                         <Form.Label>Password</Form.Label>
                         <Form.Control type='password' placeholder='Enter password' value={password} onChange={e => setPassword(e.target.value)}></Form.Control>
                     </Form.Group>
-                    <Form.Group controlId='formBasicCheckbox'>
-                        <Form.Check type='checkbox' label='Keep me logged in' value='false' chec />
+                    <Form.Group controlId='keepLoggedIn'>
+                        <Form.Check type='checkbox' label='Remember ' />
                     </Form.Group>
                     <Button type='submit' variant='primary'>Sign In</Button>
                 </Form>
