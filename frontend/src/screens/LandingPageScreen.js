24b708d005924555234e4baac091199fb598cf75
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 12:25:51 +0530

    update: master

--- frontend/src/screens/LandingPageScreen.js
+++ frontend/src/screens/LandingPageScreen.js
@@ -28,6 +28,13 @@ const LandingPageScreen = () => {
                             </Col>
                         ))
                     }
+                    <Card className="my-3 py-3 rounded">
+                        <Card.Img src={feature.image} variant="top"></Card.Img>
+                        <Card.Body>
+                            <Card.Title as='div'><strong>{feature.name}</strong></Card.Title>
+                            <Card.Text as='h4'>${feature.description}</Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Row>
             }
         </div>
