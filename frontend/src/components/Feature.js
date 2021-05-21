2761474e5c143df0266bc1bc95de869a73cea420
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 12:38:48 +0530

    update: master

--- frontend/src/components/Feature.js
+++ frontend/src/components/Feature.js
@@ -8,7 +8,7 @@ const Feature = ({ feature }) => {
             <Card.Img src={feature.image} variant="top"></Card.Img>
             <Card.Body>
                 <Card.Title as='div'><strong>{feature.name}</strong></Card.Title>
-                <Card.Text as='h4'>${feature.description}</Card.Text>
+                <Card.Text as='p'>{feature.description}</Card.Text>
             </Card.Body>
         </Card>
     )
