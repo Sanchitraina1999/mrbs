35e4334c687aa6762a188d8446234cce903142c2
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 12:41:00 +0530

    update: master

--- frontend/src/components/Feature.js
+++ frontend/src/components/Feature.js
@@ -5,7 +5,7 @@ import { Card } from "react-bootstrap"
 const Feature = ({ feature }) => {
     return (
         <Card className="my-3 py-3 rounded">
-            <Card.Img src={feature.image} variant="top"></Card.Img>
+            <Card.Img src={feature.image} variant='top'></Card.Img>
             <Card.Body>
                 <Card.Title as='div'><strong>{feature.name}</strong></Card.Title>
                 <Card.Text as='p'>{feature.description}</Card.Text>
