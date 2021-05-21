77e1db3affe270c2a18fa735703e77cbe48ead56
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 11:40:09 +0530

    update: master

--- backend/data/features.js
+++ backend/data/features.js
@@ -2,20 +2,17 @@ const features = [
     {
         name: 'Effortlessly manage reservations and services',
         image: '/images/asset0.svg',
-        description:
-            'With MBRS, the most complex of reservations become simple to manage and monitor for you, even across different time zones and locations.',
+        description: 'With MBRS, the most complex of reservations become simple to manage and monitor for you, even across different time zones and locations.',
     },
     {
         name: 'Manage meetings on the move',
         image: '/images/asset1.svg',
-        description:
-            'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        description: ''
     },
     {
         name: 'Get workspace insight',
         image: '/images/asset2.svg',
-        description:
-            'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        description: ''
     }
 ]
 
