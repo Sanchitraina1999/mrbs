54633dd6926d154495c3b48188dc374d7743bd2c
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 11:08:36 +0530

    update: master

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import featureReducer from '../reducers/featureReducer'
+
 const reducer = combineReducers({
-    // productList: productReducer,
+    featureList: featureReducer,
     // productDetails: productDetailsReducer,
     // cart: cartReducer,
     // userLogin: userLoginReducer,
