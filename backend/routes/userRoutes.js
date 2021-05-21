7d96b733d27e360ab214f53ea09ee4f8483f1aac
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 23:48:02 +0530

    update: master

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import { authUser,registerUser } from '../controllers/userController.js'
-// import { protect } from '../middleware/authMiddleware.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
 router.route('/').post(registerUser)
