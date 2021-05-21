878bbc5a4962d964e31c266966662ac067d7ba7a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 21:11:49 +0530

    update: master

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { config } from 'dotenv'
 import colors from 'colors'
-import Recaptcha from 'express-recaptcha'
+import Recaptcha from 'express-recaptcha
 
 import connectDB from "./config/db.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
@@ -14,9 +14,6 @@ const app = express()
 connectDB()
 
 app.use(express.json())
-
-
-var recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
 
 const PORT = process.env.PORT
 
