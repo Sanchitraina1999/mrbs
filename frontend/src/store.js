05e367978d430779dae258737791368550e552e1
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 13:51:54 +0530

    update: master

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import featureReducer from '../src/reducers/featureReducer'
-import userReducer from '../src/reducers/userReducer'
+import userRegisterReducer from '../src/reducers/userReducer'
 
 const reducer = combineReducers({
     featureList: featureReducer,
