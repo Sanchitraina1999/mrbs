3ba18f464180f9000722ffdfd7143e0f0c5f6b73
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 11:37:59 +0530

    update: master

--- backend/data/features.js
+++ backend/data/features.js
@@ -12,7 +12,7 @@ const features = [
             'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     },
     {
-        name: 'Airpods Wireless Bluetooth Headphones',
+        name: 'Get workspace insight',
         image: '/images/airpods.jpg',
         description:
             'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
