7ce55797a7b91ea076a5be295edce69233dc2389
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 13:47:39 +0530

    update: master

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,34 +3,21 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import featureReducer from '../src/reducers/featureReducer'
+import from '../src/reducers/userRed'
 
 const reducer = combineReducers({
     featureList: featureReducer,
-    // productDetails: productDetailsReducer,
-    // cart: cartReducer,
     // userLogin: userLoginReducer,
     // userRegister: userRegisterReducer,
     // userDetails: userDetailsReducer,
-    // userUpdateProfile: userUpdateProfileReducer,
-    // orderCreate: orderCreateReducer,
-    // orderDetails: orderDetailsReducer,
-    // orderPay: orderPayReducer
+    // userUpdateProfile: userUpdateProfileReducer
 })
 
-// const cartItemsFromStorage = localStorage.getItem('cartItems') ?
-// JSON.parse(localStorage.getItem('cartItems')) : []
 
 // const userInfoFromStorage = localStorage.getItem('userInfo') ?
 //     JSON.parse(localStorage.getItem('userInfo')) : null
 
-// const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
-//     JSON.parse(localStorage.getItem('shippingAddress')) : []
-
 const initialState = {
-    // cart: {
-    //     cartItems: cartItemsFromStorage,
-    //     shippingAddress: shippingAddressFromStorage
-    // },
     // userLogin: {
     //     userInfo: userInfoFromStorage
     // }
