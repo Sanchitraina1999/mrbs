48493bf18a54367b55d1cb3bc6b77bd200466d97
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 11:39:04 +0530

    update: master

--- backend/data/features.js
+++ backend/data/features.js
@@ -1,19 +1,19 @@
 const features = [
     {
         name: 'Effortlessly manage reservations and services',
-        image: '/images/airpods.jpg',
+        image: '/images/asset0.svg',
         description:
-            'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+            'With MBRS, the most complex of reservations become simple to manage and monitor for you, even across different time zones and locations.',
     },
     {
         name: 'Manage meetings on the move',
-        image: '/images/airpods.jpg',
+        image: '/images/asset1.svg',
         description:
             'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     },
     {
         name: 'Get workspace insight',
-        image: '/images/airpods.jpg',
+        image: '/images/asset2.svg',
         description:
             'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     }
