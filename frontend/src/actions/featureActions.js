84925e5981ae10d4588ae84ae7663c2f3857a5de
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 11:04:15 +0530

    update: master

--- frontend/src/actions/featureActions.js
+++ frontend/src/actions/featureActions.js
@@ -1,31 +1,17 @@
 import axios from 'axios';
 import { 
-    PRODUCT_LIST_REQUEST, 
-    PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAIL,
-    PRODUCT_DETAILS_REQUEST, 
-    PRODUCT_DETAILS_SUCCESS, 
-    PRODUCT_DETAILS_FAIL 
+    FEATURE_LIST_REQUEST, 
+    FEATURE_LIST_SUCCESS, 
+    FEATURE_LIST_FAIL,
 } from '../constants/productConstants';
 
-export const listProducts = () => async (dispatch) => {
+export const listFeatures = () => async (dispatch) => {
     try {
-        dispatch({ type: PRODUCT_LIST_REQUEST });
+        dispatch({ type: FEATURE_LIST_REQUEST });
         const { data } = await axios.get('/api/products');
-        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+        dispatch({ type: FEATURE_LIST_SUCCESS, payload: data });
     }
     catch (error) {
-        dispatch({ type: PRODUCT_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
-    }
-};
-
-export const listProductDetails = (id) => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST });
-        const { data } = await axios.get(`/api/products/${id}`);
-        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
-    }
-    catch (error) {
-        dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
+        dispatch({ type: FEATURE_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
     }
 };
