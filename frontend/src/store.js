c33fd99f1aeb722cc35f849c691faf55c21e75b9
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-21 13:45:28 +0530

    update: master

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import featureReducer from '../src/reducers/featureReduer'
+import featureReducer from '../src/reducers/featureReducer'
 
 const reducer = combineReducers({
     featureList: featureReducer,
