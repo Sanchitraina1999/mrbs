af8466640fde679d47de186c2a152d512a29ffd9
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 19:58:13 +0530

    update: master

--- backend/data/meetingRooms.js
+++ backend/data/meetingRooms.js
@@ -42,27 +42,6 @@ const meetingrooms = [
         ]
     },
     {
-        roomName: 'Mos Eisley Cantina',
-        image: '/images/room3.jpg',
-        location: {
-            address: 'Manish Market, Dadar',
-            city: 'Mumbai'
-        },
-        pointOfContact: {
-            name: 'Nikhil Kulkarni',
-            mobile: '+91 7889706221',
-            email: 'nikhil2001@gmail.com'
-        },
-        bookedTimes: [
-            {
-                startDate: 'Sat, 22 May 2021 08:54:22 GMT',
-                endDate: 'Sat, 22 May 2021 08:54:22 GMT',
-                bookedBy: '60a87d34068c169b2810942b',
-                purposeOfBooking: 'Student Branch Meeting'
-            }
-        ]
-    },
-    {
         roomName: 'Spider Skull Island',
         image: '/images/room4.jpg',
         location: {
@@ -73,6 +52,27 @@ const meetingrooms = [
             name: 'Rajwinder Singh',
             mobile: '+91 9682666878',
             email: 'rajwinder879@gmail.com'
+        },
+        bookedTimes: [
+            {
+                startDate: 'Sat, 22 May 2021 08:54:22 GMT',
+                endDate: 'Sat, 22 May 2021 08:54:22 GMT',
+                bookedBy: '60a87d34068c169b2810942b',
+                purposeOfBooking: 'Coding Conclave'
+            }
+        ]
+    },
+    {
+        roomName: 'Mos Eisley Cantina',
+        image: '/images/room3.jpg',
+        location: {
+            address: 'Manish Market, Dadar',
+            city: 'Mumbai'
+        },
+        pointOfContact: {
+            name: 'Nikhil Kulkarni',
+            mobile: '+91 7889706221',
+            email: 'nikhil2001@gmail.com'
         },
         bookedTimes: [
             {
