6a64ad29b3d28bee0048f19de0c95d611414e05a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 20:49:06 +0530

    update: master

--- frontend/src/screens/HomePageScreen.js
+++ frontend/src/screens/HomePageScreen.js
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-const HomeScreen = ({}) => {
+const HomeScreen = ({ }) => {
     const dispatch = useDispatch()
     const meetingRoomList = useSelector(state => state.meetingRoomList)
     const { loading, error, meetingRooms } = meetingRoomList
@@ -19,18 +19,22 @@ const HomeScreen = ({}) => {
     }, [dispatch])
     return (
         <>
-            <h1>MEETING ROOMS:</h1>
-            {loading ? <Loader /> : error
-                ? <Message variant='danger'>{error}</Message>
-                : <Row>
+            {userInfo ? (
+                <h1>MEETING ROOMS:</h1>
+                {loading ? (<Loader />) : (error
+                ? (<Message variant='danger'>{error}</Message>)
+                : (<Row>
                     {
                         meetingRooms.map(room => (
                             <Col key={room._id} sm={12} md={6} lg={4} xl={3}>
-                                <MeetingRoom room={room}/>
+                                <MeetingRoom room={room} />
                             </Col>
                         ))
                     }
-                </Row>
+                </Row>))
+            })
+                :
+            <Redirect to='/login'></Redirect>
             }
         </>
     )
