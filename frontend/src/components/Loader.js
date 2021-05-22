d95ea1777bf23d5b560d7e3a434181db5c578023
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 23:57:44 +0530

    update: master

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
+import React from 'react'
+import { Spinner } from 'react-bootstrap'
 
 const Loader = () => {
   return (
-    <Spinner animation="border" role="status" style={{
+    <Spinner animation='border' role='status' style={{
       width: '100px',
       height: '100px',
       margin: '25vh auto',
       display: 'block',
       color: 'black'
     }}>
-      <span className="sr-only">Loading...</span>
+      <span className='sr-only'>Loading...</span>
     </Spinner>
-  );
+  )
 }
 
-export default Loader;
+export default Loader
--- frontend/src/screens/BookingPageScreen.js
+++ frontend/src/screens/BookingPageScreen.js
@@ -44,13 +44,15 @@ const BookingPageScreen = ({ history, match }) => {
                                         </ListGroup.Item>
                                         <ListGroup.Item>
                                             For more details:
-                                            {/* {`${meetingRoom.pointOfContact.name} ${meetingRoom.pointOfContact.mobile} ${meetingRoom.pointOfContact.email}`} */}
+                                            {`${meetingRoom.pointOfContact.name} ${meetingRoom.pointOfContact.mobile} ${meetingRoom.pointOfContact.email}`}
                                         </ListGroup.Item>
                                     </ListGroup>
                                     <Card className='card bg-dark mb-3'>
                                         <ListGroup variant='flush'>
                                             <ListGroup.Item>
-
+                                                <Form.Control>
+                                                    
+                                                </Form.Control>
                                             </ListGroup.Item>
                                         </ListGroup>
                                     </Card>
