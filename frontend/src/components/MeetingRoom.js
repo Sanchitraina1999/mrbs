d2c65bddc866184798f92224f6a5cc5c85d0ca77
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 19:44:08 +0530

    update: master

--- frontend/src/components/MeetingRoom.js
+++ frontend/src/components/MeetingRoom.js
@@ -1,15 +1,15 @@
-import React from 'react';
-// import { Link } from "react-router-dom";
-import { Card } from "react-bootstrap";
+import React from 'react'
+// import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 const MeetingRoom = ({ room }) => {
     return (<>
         {/*  <Link to={`/products/${product._id}`}> */}
-            <Card className="my-3 py-3 rounded btn-outline-dark">
-                <Card.Img src={room.image} variant="top"></Card.Img>
+            <Card className='my-3 py-3 rounded btn-outline-dark'>
+                <Card.Img src={room.image} variant='top'></Card.Img>
                 <Card.Body>
                     <Card.Title as='div'><strong>{room.roomName}</strong></Card.Title>
                     <Card.Subtitle className='mb-2 text-muted'>{`${room.location.address}, ${room.location.city}`}</Card.Subtitle>
-                    <Card className="mt-3 py-2 rounded btn-outline-warning">
+                    <Card className='mt-3 py-2 rounded'>
                         <Card.Body>
                             <Card.Text as='h5' className='text-info'>{room.pointOfContact.name}</Card.Text>
                             <Card.Text className='text-muted'>{room.pointOfContact.mobile}</Card.Text>
