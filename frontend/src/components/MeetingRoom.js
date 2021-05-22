06d7d330ffc58661735e97a920340c002acbc268
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 18:44:25 +0530

    update: master

--- frontend/src/components/MeetingRoom.js
+++ frontend/src/components/MeetingRoom.js
@@ -3,13 +3,12 @@ import React from 'react';
 import { Card } from "react-bootstrap";
 const MeetingRoom = ({ room }) => {
     return (<>
-        {room}
         {/*  <Link to={`/products/${product._id}`}> */}
             <Card className="my-3 py-3 rounded">
                 <Card.Img src={room.image} variant="top"></Card.Img>
                 <Card.Body>
                     <Card.Title as='div'><strong>{room.roomName}</strong></Card.Title>
-                    <Card.Text as='h4'>${room.pointOfContact.name}</Card.Text>
+                    <Card.Text as='h4'>{room.pointOfContact.name}</Card.Text>
                 </Card.Body>
             </Card>
         {/*  </Link > */}
