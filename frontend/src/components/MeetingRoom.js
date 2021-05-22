f072145e8f5362da87396c9fa73a97d5f1fe4094
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 19:41:53 +0530

    update: master

--- frontend/src/components/MeetingRoom.js
+++ frontend/src/components/MeetingRoom.js
@@ -4,7 +4,7 @@ import { Card } from "react-bootstrap";
 const MeetingRoom = ({ room }) => {
     return (<>
         {/*  <Link to={`/products/${product._id}`}> */}
-            <Card className="my-3 py-3 rounded btn-dark">
+            <Card className="my-3 py-3 rounded btn-outline-dark">
                 <Card.Img src={room.image} variant="top"></Card.Img>
                 <Card.Body>
                     <Card.Title as='div'><strong>{room.roomName}</strong></Card.Title>
