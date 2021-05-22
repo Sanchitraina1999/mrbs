c2da980871f5963d523c09e22ca5eedf3ae7ac55
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 14:42:26 +0530

    update: master

---
+++ frontend/src/actions/meetingRoomActions.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { 
+    FEATURE_LIST_REQUEST, 
+    FEATURE_LIST_SUCCESS, 
+    FEATURE_LIST_FAIL,
+} from '../constants/featureConstants'
+
+export const listFeatures = () => async (dispatch) => {
+    try {
+        dispatch({ type: FEATURE_LIST_REQUEST })
+        const { data } = await axios.get('/api/features')
+        dispatch({ type: FEATURE_LIST_SUCCESS, payload: data })
+    }
+    catch (error) {
+        dispatch({ type: FEATURE_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
+    }
+}
