725f95a04527514a822e1a9d0d84110065749c36
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 16:36:08 +0530

    update: master

--- frontend/src/actions/meetingRoomActions.js
+++ frontend/src/actions/meetingRoomActions.js
@@ -7,8 +7,26 @@ import {
 
 export const listMeetingRooms = () => async (dispatch) => {
     try {
+
+        
+        
+        const { data } = await axios.post(
+            '/api/users/',
+            { username, email, password },
+            config
+        )
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
+        dispatch({ type: USER_LOGIN_SUCCESS, loginSuccess: true, payload: data })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
         dispatch({ type: MEETING_ROOM_LIST_REQUEST })
-        const { data } = await axios.get('/api/meetingRooms')
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer + 
+            }
+        }
+        const { data } = await axios.get('/api/meetingRooms',config)
         dispatch({ type: MEETING_ROOM_LIST_SUCCESS, payload: data })
     }
     catch (error) {
