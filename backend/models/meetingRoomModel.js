823dbb721d91a4fc44e214960163e59b561ff981
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 13:47:07 +0530

    update: master

--- backend/models/meetingRoomModel.js
+++ backend/models/meetingRoomModel.js
@@ -24,48 +24,7 @@ const meetingRoomSchema = mongoose.Schema({
         city: { type: String, required: true },
         postalCode: { type: String, required: true },
         country: { type: String, required: true }
-    },
-    paymentMethod: {
-        type: String,
-        require: true
-    },
-    paymentResult: {
-        id: { type: String },
-        status: { type: String },
-        update_time: { type: String },
-        email_address: { type: String },
-    },
-    taxPrice: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    shippingPrice: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    totalPrice: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    isPaid: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    paidAt: {
-        type: Date
-    },
-    isDelivered: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    deliveredAt: {
-        type: Date
-    },
+    }
 }, {
     timestamps: true
 })
