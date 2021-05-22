1577ba14375b7dffb8409ed297bd0b2235463943
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 20:39:30 +0530

    update: master

--- frontend/src/screens/HomePageScreen.js
+++ frontend/src/screens/HomePageScreen.js
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-const HomeScreen = () => {
+const HomeScreen = ({location,history}) => {
     const dispatch = useDispatch()
     const meetingRoomList = useSelector(state => state.meetingRoomList)
     const { loading, error, meetingRooms } = meetingRoomList
