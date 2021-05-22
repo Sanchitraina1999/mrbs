46c7079505135544ba07be2cc26ce6390ce53dc7
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 13:38:26 +0530

    update: master

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/features', featureRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/meetings', meetingRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
