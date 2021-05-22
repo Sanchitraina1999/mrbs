acf3924e64bc8f1f5798700e3f6b65f5d77c5527
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 22:48:21 +0530

    update: master

--- backend/routes/meetingRoomRoutes.js
+++ backend/routes/meetingRoomRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { getMeetingRooms } from '../controllers/meetingRoomController.js'
+import { getMeetingRooms,getMeetingRoomById } from '../controllers/meetingRoomController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').get(protect, getMeetingRooms)
-router.route('/:id').get(protect,getMeetingRoomById);
+router.route('/:id').get(protect,getMeetingRoomById)
 
 export default router
