7a40732d3e3245d1b31fe5e1693a6306d88e06e0
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 19:51:41 +0530

    update: master

--- backend/data/meetingRooms.js
+++ backend/data/meetingRooms.js
@@ -3,8 +3,29 @@ const meetingrooms = [
         roomName: 'Executive Playground',
         image: '/images/room1.jpg',
         location: {
-            address: 'Katraj',
+            address: 'Survey No.27, Katraj',
             city: 'Pune'
+        },
+        pointOfContact: {
+            name: 'Sanchit Raina',
+            mobile: '+91 9149659875',
+            email: 'sanchitraina1999@gmail.com'
+        },
+        bookedTimes: [
+            {
+                startDate: 'Sat, 22 May 2021 08:54:22 GMT',
+                endDate: 'Sat, 22 May 2021 08:54:22 GMT',
+                bookedBy: '60a87d34068c169b2810942b',
+                purposeOfBooking: 'AI conference'
+            }
+        ]
+    },
+    {
+        roomName: 'Thunderdome',
+        image: '/images/room2.jpg',
+        location: {
+            address: 'Thane',
+            city: 'Mumbai'
         },
         pointOfContact: {
             name: 'Sanchit Raina',
