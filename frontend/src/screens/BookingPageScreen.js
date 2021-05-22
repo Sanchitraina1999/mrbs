e8d61e899e440b79a68d91b6f011a5aa377f62f3
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 23:01:25 +0530

    update: master

--- frontend/src/screens/BookingPageScreen.js
+++ frontend/src/screens/BookingPageScreen.js
@@ -2,27 +2,20 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listMeetingRoomDetails } from '../actions/meetingRoomActions'
 
 const BookingPageScreen = ({ history, match }) => {
 
-    const [qty, setQty] = useState(1)
-
     const dispatch = useDispatch()
-    const productDetails = useSelector(state => state.productDetails)
 
-    const { loading, error, product } = productDetails
+    const meetingRoomDetails = useSelector(state => state.meetingRoomDetails)
+    const { loading, error, meetingRoom } = meetingRoomDetails
 
     useEffect(() => {
-        dispatch(listProductDetails(match.params.id))
+        dispatch(listMeetingRoomDetails(match.params.id))
     }, [dispatch, match])
-
-    const addToCartHandler = () => {
-        history.push(`/cart/${match.params.id}?qty=${qty}`)
-    }
 
     return (
         <>
@@ -32,70 +25,20 @@ const BookingPageScreen = ({ history, match }) => {
             {
                 loading ? <Loader />
                     : error ? (<Message variant='danger'>{error}</Message>)
-                        : (<Row>
-                            <Col md={6}>
-                                <Image src={product.image} alt={product.name} fluid />
-                            </Col>
-                            <Col md={3}>
-                                <ListGroup variant='flush'>
-                                    <ListGroup.Item >
-                                        {product.name}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <Rating product={product} />({product.numReviews})
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        Price: ${product.price}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        Description: {product.description}
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Col>
-                            <Col md={3}>
-                                <Card className='card bg-dark mb-3'>
+                        : (
+                            <Row>
+                                <Col md={6}>
+                                    <Image src={meetingRoom.image} alt={meetingRoom.name} fluid />
+                                </Col>
+                                <Col md={3}>
                                     <ListGroup variant='flush'>
-                                        <ListGroup.Item>
-                                            <Row>
-                                                <Col>Price:</Col>
-                                                <Col><strong>${product.price}</strong></Col>
-                                            </Row>
-                                        </ListGroup.Item>
-                                        <ListGroup.Item>
-                                            <Row>
-                                                <Col>Status:</Col>
-                                                <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
-                                            </Row>
-                                        </ListGroup.Item>
-
-                                        {
-                                            product.countInStock > 0 && (
-                                                <ListGroup.Item>
-                                                    <Row>
-                                                        <Col>Qty</Col>
-                                                        <Col>
-                                                            <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
-                                                                {[...Array(product.countInStock).keys()].map((x) => (
-                                                                    <option key={x + 1} value={x + 1}>
-                                                                        {x + 1}
-                                                                    </option>
-                                                                ))}
-                                                            </Form.Control>
-                                                        </Col>
-                                                    </Row>
-                                                </ListGroup.Item>
-                                            )
-                                        }
-
-                                        <ListGroup.Item>
-                                            <Button onClick={addToCartHandler} className='btn btn-block' type='button' disabled={product.countInStock === 0}>
-                                                Add to Cart
-                                            </Button>
+                                        <ListGroup.Item >
+                                            {meetingRoom.name}
                                         </ListGroup.Item>
                                     </ListGroup>
-                                </Card>
-                            </Col>
-                        </Row>)
+                                </Col>
+                            </Row>
+                        )
             }
         </>
     )
