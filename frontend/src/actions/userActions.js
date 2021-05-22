fed4b1ef0baaa1a198d1446e434ad5ddc74c9847
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 14:58:46 +0530

    update: master

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -64,7 +64,7 @@ export const register = (username, email, password) => async (dispatch) => {
             config
         )
         dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
-        dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+        dispatch({ type: USER_LOGIN_SUCCESS, loginSuccess: true, payload: data })
         localStorage.setItem('userInfo', JSON.stringify(data))
     }
     catch (error) {
