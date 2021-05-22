f54b357205b59274993bdab2749bd4da66580546
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 19:53:53 +0530

    update: master

--- backend/data/meetingRooms.js
+++ backend/data/meetingRooms.js
@@ -40,6 +40,27 @@ const meetingrooms = [
                 purposeOfBooking: 'Company Meeting'
             }
         ]
+    },
+    {
+        roomName: 'Mos Eisley Cantina',
+        image: '/images/room3.jpg',
+        location: {
+            address: 'Manish Market, Dadar',
+            city: 'Mumbai'
+        },
+        pointOfContact: {
+            name: 'Sahil Singh',
+            mobile: '+91 7006683179',
+            email: 'sahilbhau74@gmail.com'
+        },
+        bookedTimes: [
+            {
+                startDate: 'Sat, 22 May 2021 08:54:22 GMT',
+                endDate: 'Sat, 22 May 2021 08:54:22 GMT',
+                bookedBy: '60a87d34068c169b2810942b',
+                purposeOfBooking: 'Company Meeting'
+            }
+        ]
     }
 ]
 
