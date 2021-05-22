c328f96cc6344f378cb0f6d93ebd741b926e7f91
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 14:46:45 +0530

    update: master

--- frontend/src/screens/HomePageScreen.js
+++ frontend/src/screens/HomePageScreen.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react"
+import { Row, Col } from "react-bootstrap"
+
+import { useDispatch, useSelector } from "react-redux"
+
+import Feature from "../components/Feature"
+import Loader from "../components/Loader"
+import Message from "../components/Message"
+import { listFeatures } from "../actions/featureActions"
 
 const HomePageScreen = () => {
+    const dispatch = useDispatch()
+    const featureList = useSelector(state => state.featureList)
+    const { loading, error, features } = featureList
+    useEffect(() => {
+        dispatch(listFeatures())
+    }, [dispatch])
     return (
         <div>
-            HomePageScreen
+            <h1>FEATURES:</h1>
+            {loading ? <Loader /> : error
+                ? <Message variant='danger'>{error}</Message>
+                : <Row>
+                    {
+                        features.map(feature => (
+                            <Col key={feature._id}>
+                                <Feature feature={feature} />
+                            </Col>
+                        ))
+                    }
+                </Row>
+            }
         </div>
     )
 }
