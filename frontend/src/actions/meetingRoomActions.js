d70bacf978ba1faed2cae215831dab10fcb8ccc2
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-22 22:54:51 +0530

    update: master

--- frontend/src/actions/meetingRoomActions.js
+++ frontend/src/actions/meetingRoomActions.js
@@ -25,3 +25,14 @@ export const listMeetingRooms = () => async (dispatch,getState) => {
         dispatch({ type: MEETING_ROOM_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
     }
 }
+
+export const listMeetingRoomDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: MEETING_ROOM_DETAILS_REQUEST });
+        const { data } = await axios.get(`/api/meetingRooms/${id}`);
+        dispatch({ type: MEETING_ROOM_DETAILS_SUCCESS, payload: data });
+    }
+    catch (error) {
+        dispatch({ type: MEETING_ROOM_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
+    }
+};
