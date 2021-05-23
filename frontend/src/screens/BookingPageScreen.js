0f6703c38922262e74d5daac9b254b27016fd8d3
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 10:15:18 +0530

    update: master

--- frontend/src/screens/BookingPageScreen.js
+++ frontend/src/screens/BookingPageScreen.js
@@ -34,7 +34,7 @@ const BookingPageScreen = ({ history, match }) => {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        var currentDateTime = moment(new Date()).format()
+        var currentDateTime = moment(new Date()).format('yyyy-MM-DD')
         var startDateTime = moment(new Date()).format()
         var endDateTime = moment(new Date()).format()
         { console.log(currentDateTime) }
