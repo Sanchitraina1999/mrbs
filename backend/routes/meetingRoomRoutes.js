24a92a6b9cac65acdf735e263d129ef486a7c311
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 20:33:36 +0530

    update: master

--- backend/routes/meetingRoomRoutes.js
+++ backend/routes/meetingRoomRoutes.js
@@ -6,5 +6,6 @@ import { protect } from '../middleware/authMiddleware.js'
 router.route('/').get(protect, getMeetingRooms)
 router.route('/:id').get(protect, getMeetingRoomById)
 router.route('/availability/:id').post(protect, getAvailabilityById)
-router.route('/book/:id').patch(protect, bookMeetingRoom)
+router.route('/book/:id').put(protect, bookMeetingRoom)
+
 export default router
