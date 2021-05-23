1c1b07dceeff92cf254fd2b2ec012a3f9840db7e
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-24 01:38:17 +0530

    update: master

--- backend/controllers/meetingRoomController.js
+++ backend/controllers/meetingRoomController.js
@@ -119,6 +119,7 @@ const getMyMeetings = asyncHandler(async (req, res) => {
 //@access Private
 const deleteMeeting = asyncHandler(async (req, res) => {
     const { roomId } = req.body
+    console.log(roomId)
     const meetingRoom = await MeetingRoom.findById(roomId)
     if (meetingRoom) {
         meetingRoom.bookedTimes = meetingRoom.bookedTimes.filter((booking) => booking._id != req.params.id)
