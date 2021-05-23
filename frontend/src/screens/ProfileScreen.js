cc03cbf0403d0f2378fd64db51a111c3aa3d7de8
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 12:25:47 +0530

    update: master

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Form, Button, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Form, Button, Row, Col, ListGroup } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { ToastContainer, toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
