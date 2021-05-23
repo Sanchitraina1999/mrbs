dc8ac8e8dbaa8eed2a9ef165d6ee29576895d2e6
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 11:30:30 +0530

    update: master

--- frontend/src/screens/BookingPageScreen.js
+++ frontend/src/screens/BookingPageScreen.js
@@ -45,6 +45,9 @@ const BookingPageScreen = ({ history, match }) => {
             setMessage('End Time can not be before Start Time')
         else if(purposeOfBooking.replace(/\s/g,'').length<=0)
             setMessage('Purpose of Meeting cannot be empty')
+        {console.log(currentDateTime)}
+        {console.log(startDateTime)}
+        {console.log(endDateTime)}
         // else
             // dispatch()
     }
