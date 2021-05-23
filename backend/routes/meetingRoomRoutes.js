b36334e5d88a8a7a3816d192bc9c265fbbb60271
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 21:11:33 +0530

    update: master

--- backend/routes/meetingRoomRoutes.js
+++ backend/routes/meetingRoomRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { getMeetingRooms, getMeetingRoomById, getAvailabilityById, bookMeetingRoom } from '../controllers/meetingRoomController.js'
+import { getMeetingRooms, getMeetingRoomById, getAvailabilityById, bookMeetingRoom, getMyMeetings } from '../controllers/meetingRoomController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').get(protect, getMeetingRooms)
 router.route('/:id').get(protect, getMeetingRoomById)
 router.route('/availability/:id').post(protect, getAvailabilityById)
 router.route('/book/:id').put(protect, bookMeetingRoom)
+router.route('/myMeetings/:id').get(protect, getMyMeetings)
 
 export default router
