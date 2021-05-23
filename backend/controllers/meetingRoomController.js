f5ece46c4364f21b1f53da5b0fad92d59b051d00
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 23:20:34 +0530

    update: master

--- backend/controllers/meetingRoomController.js
+++ backend/controllers/meetingRoomController.js
@@ -105,7 +105,7 @@ const getMyMeetings = asyncHandler(async (req, res) => {
             })
         })
         myMeetings.sort((a, b) => (a.startDateTime > b.startDateTime) ? 1 : ((b.startDateTime > a.startDateTime) ? -1 : 0))
-        myMeetings.filter((meets)=> meets.endDateTime > today)
+        myMeetings=myMeetings.filter((meets)=> meets.endDateTime > today)
         res.json(myMeetings)
     }
     else {
