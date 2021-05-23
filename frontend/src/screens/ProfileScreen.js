40fffd5ba8d0c8798ef22703abba4a9bc9f44953
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 12:29:02 +0530

    update: master

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -114,11 +114,11 @@ const ProfileScreen = ({ location, history }) => {
                         {myMeetings.map((item, index) => (
                             <ListGroup.Item key={index}>
                                 <Row>
-                                    {/* <Col md={2}>
+                                    <Col md={2}>
                                         <Link to={`/meetingRooms/${item.room}`}>
                                             Room Name: {item.roomName}
                                         </Link>
-                                    </Col> */}
+                                    </Col>
                                     <Col md={2}>
                                         Start Date:{item.startDateTime.split('T')[0]}{', '}
                                         Start Time: {item.startDateTime.split('T')[1]}
