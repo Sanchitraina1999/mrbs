56662364cda84fe5858597afd7673951c811e861
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-23 14:28:13 +0530

    update: master

--- frontend/src/actions/meetingRoomActions.js
+++ frontend/src/actions/meetingRoomActions.js
@@ -57,7 +57,7 @@ export const getAvailablityOfMeetingRoom = (id, startDateTime, endDateTime) => a
                 Authorization: `Bearer ${userInfo.token}`
             }
         }
-        const { data } = await axios.get(`/api/meetingRooms/${id}`,{id, startDateTime, endDateTime}, config)
+        const { data } = await axios.get(`/api/meetingRooms/availability/${id}`,{startDateTime, endDateTime}, config)
         dispatch({ type: MEETING_ROOM_AVAILABILITY_SUCCESS, payload: data })
     }
     catch (error) {
