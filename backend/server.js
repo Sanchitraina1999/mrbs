2adff0341bbda69f82321ecfeaf800bdca175108
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-24 02:50:21 +0530

    :fire:

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { config } from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
+import path from 'path'
 
 import connectDB from "./config/db.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
@@ -15,16 +17,28 @@ connectDB()
 
 app.use(express.json())
 
-const PORT = process.env.PORT
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
-app.get('/', (req, res) => {
-    console.log(typeof process.env.MONGO_URI)
-    res.send("API is running")
-})
+const PORT = process.env.PORT || 5000
 
 app.use('/api/features', featureRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/meetingRooms', meetingRoomRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+else {
+    app.get('/', (req, res) => {
+        console.log(typeof process.env.MONGO_URI)
+        res.send("API is running")
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
